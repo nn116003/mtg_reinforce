3f4e89e55c9bf375f39e177b6895c1bb04f1d1df
Ubuntu <ubuntu@ip-172-31-24-67.us-west-2.compute.internal>
2019-07-19 02:37:16 +0000

    add model

---
+++ model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class CardEncoder(nn.Module):
+    def __init__(self, in_d, out_d):
+        super(CardEncoder, self).__init__()
+
+    def forward(self, x):
+        pass
+
+class GameEncoder(nn.Module):
+    def __init__(self, in_d, out_d):
+        super(CardEncoder, self).__init__()
+
+    def forward(self, x):
+        pass
+
+    
+        
